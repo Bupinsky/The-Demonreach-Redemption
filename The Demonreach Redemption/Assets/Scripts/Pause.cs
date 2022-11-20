41ebd1c26b06d71c323b3187825622d8e59c4b64
JacobSwihart <jcs6016@rit.edu>
2022-11-20 16:47:32 -0500

    Added a Main Menu, expanded pause menu
    Added Main Menu and Retry buttons to the Pause menu, added a basic Main Menu with Play and Quit buttons

--- The Demonreach Redemption/Assets/Scripts/Pause.cs
+++ The Demonreach Redemption/Assets/Scripts/Pause.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class Pause : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public static bool inButton;
     public static bool paused;
     public GameObject pauseScreen;
+    public GameObject buttonTextObject;
+    TMP_Text buttonText;
     // Start is called before the first frame update
     void Start()
     {
         inButton = false;
         paused = false;
+        buttonText = buttonTextObject.GetComponent<TMP_Text>();
+
     }
 
     // Update is called once per frame
@@ -28,12 +33,16 @@ public class Pause : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
             Time.timeScale = 1;
             paused = false;
             pauseScreen.SetActive(false);
+            buttonText.text = "Pause";
+
+
         }
         else
         {
             Time.timeScale = 0;
             paused = true;
             pauseScreen.SetActive(true);
+            buttonText.text = "Resume";
         }
     }
     public void OnPointerEnter(PointerEventData eventData)
